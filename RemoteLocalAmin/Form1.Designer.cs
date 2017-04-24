0150fe35ee0c1ff5b4a07bf2d38f79d6e5987e52
jlew0808@yahoo.com <jlew0808@yahoo.com>
2017-04-24 10:53:48 +0100

    Base form creation

--- RemoteLocalAmin/Form1.Designer.cs
+++ RemoteLocalAmin/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtBxDomain = new System.Windows.Forms.TextBox();
+            this.txtBxUserName = new System.Windows.Forms.TextBox();
+            this.txtBxServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBxDomain
+            // 
+            this.txtBxDomain.Location = new System.Drawing.Point(91, 12);
+            this.txtBxDomain.Name = "txtBxDomain";
+            this.txtBxDomain.Size = new System.Drawing.Size(148, 20);
+            this.txtBxDomain.TabIndex = 0;
+            // 
+            // txtBxUserName
+            // 
+            this.txtBxUserName.Location = new System.Drawing.Point(91, 39);
+            this.txtBxUserName.Name = "txtBxUserName";
+            this.txtBxUserName.Size = new System.Drawing.Size(148, 20);
+            this.txtBxUserName.TabIndex = 1;
+            // 
+            // txtBxServer
+            // 
+            this.txtBxServer.Location = new System.Drawing.Point(91, 65);
+            this.txtBxServer.Name = "txtBxServer";
+            this.txtBxServer.Size = new System.Drawing.Size(148, 20);
+            this.txtBxServer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Domain";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Server";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(59, 104);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(139, 23);
+            this.btnGo.TabIndex = 6;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(266, 144);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxServer);
+            this.Controls.Add(this.txtBxUserName);
+            this.Controls.Add(this.txtBxDomain);
+            this.Name = "Form1";
+            this.Text = "Remote Admin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtBxDomain;
+        private System.Windows.Forms.TextBox txtBxUserName;
+        private System.Windows.Forms.TextBox txtBxServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGo;
     }
 }
 
